5763bd417f147a01bf6a981589ab781d781e27f7
Gchaurio <17-10126@usb.ve>
2023-11-18 04:46:02 -0400

    Deletions and renames

--- parcial2/p1/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules=cythonize(
-        "1a.pyx", compiler_directives={"language_level": "3"}
-    )
-)
--- parcial2/p4/b.py
+++
@@ -1,4 +0,0 @@
-def f_cola(i,n):
-    if n == 36:
-        return n
-    else:
